7267a49e9c72a8022b1596d1f8682e839752427c
Juan <61684795+juanjq@users.noreply.github.com>
2022-04-27 23:33:28 +0200

    Update mplot.py

--- mplot.py
+++ mplot.py
@@ -5,14 +5,14 @@ import httpimport
 with httpimport.remote_repo(['plt_config'], 'https://raw.githubusercontent.com/juanjq/matplotlib_configuration/main'):
      import plt_config
         
-def plot(M,cmap = 'bwr'):        
+def plot(M, cmap='bwr', fsize=(5,5), norm=0):        
         
     plt_config.simple()
 
-    fig, ax = plt.subplots(figsize=(4,4))
+    fig, ax = plt.subplots(figsize=fsize)
 
     minmax = max([abs(np.amax(M)),abs(np.amin(M))])
-    plot   = ax.imshow(M,cmap=cmap,vmin=-minmax,vmax=minmax)
+    plot   = ax.imshow(M,cmap=cmap,vmin=-minmax,vmax=minmax, norm=colors.CenteredNorm(norm))
     plt.colorbar(plot)
 
     ax.axis('off')
