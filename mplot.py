03205cf7e71936b24cce0bc3fa113d8360eb9306
Juan <61684795+juanjq@users.noreply.github.com>
2022-04-30 15:39:20 +0200

    Update mplot.py

--- mplot.py
+++ mplot.py
@@ -8,7 +8,7 @@ with httpimport.remote_repo(['plt_config'], 'https://raw.githubusercontent.com/j
      import plt_config
 
 #plot function
-def plot(M, cmap='bwr', fsize=(5,5), norm=0):        
+def plot(M, cmap='bwr', fsize=(5,5), norm=None):        
         
     #simple configuration
     plt_config.simple()
@@ -18,7 +18,10 @@ def plot(M, cmap='bwr', fsize=(5,5), norm=0):
 #     minmax = max([abs(np.amax(M)),abs(np.amin(M))])   #,vmin=-minmax,vmax=minmax
 
     #plotting the matrix
-    plot   = ax.imshow(M,cmap=cmap, norm=colors.CenteredNorm(norm))
+    if norm != None:
+          plot = ax.imshow(M,cmap=cmap, norm=colors.CenteredNorm(norm))
+    else:
+          plot = ax.imshow(M,cmap=cmap)
     plt.colorbar(plot)
 
     ax.axis('off')
